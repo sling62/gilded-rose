a839b87dd5be89829f0346dd616966f7401d629b
sling62 <samanthaling08@hotmail.co.uk>
2022-05-19 16:47:48 +0100

    Added initial validation and template methods for updating each item

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -7,56 +7,108 @@ class GildedRose {
         this.items = items;
     }
 
+    /**
+     * Updates the quality of items at the end of each day
+     */
     public void updateQuality() {
+        //Loops over items
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
+            //Shared validation of fields on all items
+            validateItemFields(i);
+
+            // Checks to make sure item name isn't "Sulfuras, Hand of Ragnaros"
+            // If it isn't updates the quality of the item and sell by date
+            // If it is, skips update of quality and sell by date
+            if(items[i].name != "Sulfuras, Hand of Ragnaros") {
+                //checks to make sure initial quality of item is not > 50, if it is throws exception
+                if (items[i].quality > 50) {
+                    throw new IllegalArgumentException("Item quality cannot be greater than 50");
                 }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
+                //Updates quality based on category of item
+                switch (items[i].name){
+                    case "Aged Brie":
+                        updateAgedBrieItemQuality(i);
+                        break;
+                    case "Backstage passes to a TAFKAL80ETC concert":
+                        updateBackstagePassesItemQuality(i);
+                        break;
+                    case "Conjured Mana Cake":
+                        updateConjuredItemQuality(i);
+                        break;
+                    default:
+                        updateRegularItemQuality(i);
+                        break;
                 }
-            }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                //Updates sell by date to be one less day
                 items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+            } else {
+                //Checks that "Sulfuras, Hand of Ragnaros" quality value is 80, if not throws exception
+                if(items[i].quality != 80) {
+                    throw new IllegalArgumentException("Item quality must be 80 for 'Sulfuras, Hand of Ragnaros'");
                 }
             }
+        }
+    }
+
+    /**
+     * Validate initial field values of an item in items
+     * @param itemIndex index of item in items
+     */
+    private void validateItemFields(int itemIndex) {
+        if(items[itemIndex].name == null){
+            throw new NullPointerException("Item name cannot be null");
+        }
+        if (items[itemIndex].name.isBlank() || items[itemIndex].name.isEmpty()) {
+            throw new IllegalArgumentException("Item name cannot be empty");
+        }
+        if(items[itemIndex].quality <0) {
+            throw new IllegalArgumentException("Item quality cannot be less than 0");
+        }
+    }
+
+    /**
+     * Update the quality of an aged brie
+     * @param itemIndex index of aged brie in items
+     */
+    private void updateAgedBrieItemQuality(int itemIndex) {
+        //This is purely for testing
+        if(items[itemIndex].quality > 0) {
+            items[itemIndex].quality = items[itemIndex].quality -1;
+        }
+    }
+
+    /**
+     * Update the quality of a backstage pass
+     * @param itemIndex index of backstage pass in items
+     */
+    private void updateBackstagePassesItemQuality(int itemIndex) {
+        //This is purely for testing
+        if(items[itemIndex].quality > 0) {
+            items[itemIndex].quality = items[itemIndex].quality -1;
+        }
+    }
+
+    /**
+     * Update the quality of a conjured item
+     * @param itemIndex index of conjured item in items
+     */
+    private void updateConjuredItemQuality(int itemIndex) {
+        //This is purely for testing
+        if(items[itemIndex].quality > 0) {
+            items[itemIndex].quality = items[itemIndex].quality -1;
+        }
+    }
+
+    /**
+     * Update the quality of a regular item
+     * @param itemIndex index of regular item in items
+     */
+    private void updateRegularItemQuality(int itemIndex) {
+        //This is purely for testing
+        if(items[itemIndex].quality > 0) {
+            items[itemIndex].quality = items[itemIndex].quality -1;
         }
     }
 }
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -5,13 +5,107 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class GildedRoseTest {
+    private Item[] items;
+    private GildedRose app;
+
+    @Test(expected = NullPointerException.class)
+    public void givenANullItemNameIsProvided_whenUpdateQualityIsCalled_ItMustThrowANullPointerException() {
+        items = new Item[] { new Item("Aged Brie", 10, 16), new Item(null, 0, 0) };
+        app = new GildedRose(items);
+        app.updateQuality();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void givenAEmptyItemNameIsProvided_whenUpdateQualityIsCalled_ItMustThrowAIllegalArgumentException() {
+        items = new Item[] { new Item(" ", 0, 0) };
+        app = new GildedRose(items);
+        app.updateQuality();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void givenANegativeQualityIsProvided_whenUpdateQualityIsCalled_ItMustThrowAIllegalArgumentException() {
+        items = new Item[] { new Item("+5 Dexterity Vest", 0, -1) };
+        app = new GildedRose(items);
+        app.updateQuality();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void givenARegularItemWithQualityHigherThanFiftyIsProvided_whenUpdateQualityIsCalled_ItMustThrowAIllegalArgumentException() {
+        items = new Item[] { new Item("+5 Dexterity Vest", 0, 51) };
+        app = new GildedRose(items);
+        app.updateQuality();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void givenASulfurasItemWithQualityNotEightyIsProvided_whenUpdateQualityIsCalled_ItMustThrowAIllegalArgumentException() {
+        items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 50) };
+        app = new GildedRose(items);
+        app.updateQuality();
+    }
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
+    public void whenASulfurasItemWithQualityofEightyIsProvided_ItShouldNotUpdateQualityOrSellInFields() {
+        items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 20, 80) };
+        app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(80, items[0].quality);
+        assertEquals(20, items[0].sellIn);
+    }
+
+    @Test
+    public void whenAgedBrieIsProvidedAndQualityIsGreaterThanZeroButLessThanFifty_ItShouldUpdateQualityAndSellInFields() {
+        items = new Item[] { new Item("Aged Brie", 4, 2) };
+        app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Aged Brie", items[0].name);
+        assertEquals(1, items[0].quality);
+        assertEquals(3, items[0].sellIn);
+    }
+
+    @Test
+    public void whenABackstagePassIsProvidedAndQualityIsGreaterThanZeroButLessThanFifty_ItShouldUpdateQualityAndSellInFields() {
+        items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20) };
+        app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", items[0].name);
+        assertEquals(19, items[0].quality);
+        assertEquals(14, items[0].sellIn);
+    }
+
+    @Test
+    public void whenAConjuredItemIsProvidedAndQualityIsGreaterThanZeroButLessThanFifty_ItShouldUpdateQualityAndSellInFields() {
+        items = new Item[] { new Item("Conjured Mana Cake", 3, 6) };
+        app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Conjured Mana Cake", items[0].name);
+        assertEquals(5, items[0].quality);
+        assertEquals(2, items[0].sellIn);
+    }
+
+    @Test
+    public void whenARegularItemIsProvidedAndQualityIsGreaterThanZeroButLessThanFifty_ItShouldUpdateQualityAndSellInFields() {
+        items = new Item[] { new Item("+5 Dexterity Vest", 10, 20) };
+        app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("+5 Dexterity Vest", items[0].name);
+        assertEquals(19, items[0].quality);
+        assertEquals(9, items[0].sellIn);
+    }
+
+    @Test
+    public void whenARegularItemIsProvidedAndQualityIsZero_ItShouldNotUpdateQualityButUpdateSellInFields() {
+        items = new Item[] { new Item("+5 Dexterity Vest", 10, 0) };
+        app = new GildedRose(items);
+
+        app.updateQuality();
+        assertEquals("+5 Dexterity Vest", items[0].name);
+        assertEquals(0, items[0].quality);
+        assertEquals(9, items[0].sellIn);
+
+        app.updateQuality();
+        assertEquals("+5 Dexterity Vest", items[0].name);
+        assertEquals(0, items[0].quality);
+        assertEquals(8, items[0].sellIn);
     }
 
 }
